7a52a083e74b267768b3376733c2cd890a0d53a2
Karina Fedrizzi <karina.fedrizzi91@gmail.com>
2021-07-19 13:37:29 -0300

    criação do score do jogo

---
+++ Scripts/Interface.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+onready var score_label = get_node("Label")
+
+func  _ready():
+	score_label.text = str(0)
+	
+func increase_score(score):
+	score_label.text = str(score)
+
+
+
+
+
--- Scripts/Pipe.gd
+++ Scripts/Pipe.gd
@@ -2,3 +2,7 @@ extends Area2D
 
 func _physics_process(_delta): 
 	translate(Vector2(-2.5, 0))
+
+
+func _on_IncreaseScoreArea_body_entered(body):
+	body.increase_score()
